b6865a4725d5d36fe5c35e44f1e38a58ceed693b
Omoegod <Omoegod@icloud.com>
2025-04-18 16:47:03 +0300

    6

--- main.js
+++ main.js
@@ -10,6 +10,184 @@ let settingsWindow;
 let mainWindow;
 
 
+ipcMain.on('write-module', async (event, connType, msg)  => {
+
+  const config = loadConfigLocal();
+  connType = parseInt(connType, 10);
+
+  let portPath = '';
+  let portOptions = {};
+
+  if (connType === 1) {
+    
+    portPath = config.comPortOpto;
+    portOptions = {
+      baudRate: parseInt(config.baudRateOpto, 10),
+      dataBits: parseInt(config.dataBitsOpto, 10),
+      stopBits: parseInt(config.stopBitsOpto, 10),
+      parity: config.parityOpto
+    };
+
+    const port = new SerialPort({ path: portPath, ...portOptions, autoOpen: false });
+
+    try {
+      await new Promise((resolve, reject) => {
+        port.open((err) => {
+          if (err) return reject(err);
+          console.log('Port open:', portPath);
+          resolve();
+        });
+      });
+
+      const message1 = Buffer.from([0x00, 0x03, 0x45, 0x41, 0x53, 0x59, 0xc9, 0xfd]);
+      port.write(message1, (err) => {
+        if (err) {
+          console.error('Sending error:', err.message);
+        } else {
+          console.log('Data sent');
+        }
+      });
+
+      let receivedData = '';
+      const timeoutMs = 5000;
+
+      const response1 = await new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => {
+          console.warn('Waiting time expired for message 1');
+          reject(new Error('Timeout waiting for message 1'));
+        }, timeoutMs);
+
+        port.on('data', (data) => {
+          receivedData = Buffer.concat([receivedData, data]); 
+          console.log('Received data:', data.toString('hex'));
+
+          const expectedResponse = Buffer.from([0x00, 0x03, 0x45, 0x00, 0xb4, 0xc2]);
+          if (receivedData.length >= expectedResponse.length && receivedData.slice(0, expectedResponse.length).equals(expectedResponse)) {
+            clearTimeout(timeout);
+            resolve(receivedData); 
+          }
+        });
+      });
+
+      console.log('Response to message 1:', receivedData.toString('hex'));
+
+      const prefxix = "CONFIGWRITE\n"
+      const message2 = prefxix + msg;
+
+      port.write(message2, (err) => {
+        if (err) {
+          console.error('Sending error for message 2:', err.message);
+        } else {
+          console.log('Message 2 sent');
+        }
+      });
+
+      const response2 = await new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => {
+          console.warn('Waiting time expired for message 2');
+          reject(new Error('Timeout waiting for message 2'));
+        }, timeoutMs);
+
+        port.on('data', (data) => {
+          receivedData += data.toString();
+          console.log('Received data for message 2:', data.toString());
+
+          if (receivedData.length >= 10) {
+            clearTimeout(timeout);
+            resolve(receivedData);
+          }
+        });
+      });
+
+      console.log('Response to message 2:', receivedData);
+
+      port.close((err) => {
+        if (err) {
+          console.error('Error close port:', err.message);
+        } else {
+          console.log('Port closed:', portPath);
+        }
+      });
+
+      mainWindow.webContents.send('write-result', {
+        success: true,
+        data: receivedData
+      });
+
+    } catch (err) {
+      console.error('Error:', err.message);
+      mainWindow.webContents.send('write-result', {
+        success: false,
+        error: err.message
+      });
+    }
+    return;
+  } else if (connType === 2) {
+    portPath = config.comPortUsb;
+    portOptions = {
+      baudRate: parseInt(config.baudRateUsb, 10),
+      dataBits: parseInt(config.dataBitsUsb, 10),
+      stopBits: parseInt(config.stopBitsUsb, 10),
+      parity: config.parityUsb
+    };
+  } else if (connType === 3) {
+    portPath = config.comPortRf;
+    portOptions = {
+      baudRate: parseInt(config.baudRateRf, 10),
+      dataBits: parseInt(config.dataBitsRf, 10),
+      stopBits: parseInt(config.stopBitsRf, 10),
+      parity: config.parityRf
+    };
+  } else if (connType === 4) {
+    portPath = config.comPortTcp;
+    portOptions = {
+      host: config.ipTcp,
+      port: config.portTcp,
+      timeout: 2000
+    };
+
+    const client = new net.Socket();
+
+    client.setTimeout(tcpOptions.timeout);
+
+
+    client.connect(tcpOptions.port, tcpOptions.host, () => {
+      console.log(`Connect to ${tcpOptions.host}:${tcpOptions.port}`);
+
+      const prefxix = "CONFIGWRITE\n"
+
+      const message = prefxix + msg;
+      client.write(message, () => {
+        console.log('Data send to TCP');
+      });
+    });
+
+
+    client.on('timeout', () => {
+      console.warn('Waiting time expired');
+      client.destroy();
+    });
+
+    client.on('error', (err) => {
+      console.error('Error connecting to TCP server:', err.message);
+      mainWindow.webContents.send('write-result', {
+        success: false,
+        error: err.message
+      });
+    });
+
+    client.on('close', () => {
+      console.log('TCP connection closed');
+    });
+
+    return;
+  } else {
+    console.error('Unknown connection type:', connType);
+    return;
+  }
+});
+
+
 ipcMain.on('read-module', async (event, connType) => {
   const config = loadConfigLocal();
 
@@ -57,17 +235,18 @@ ipcMain.on('read-module', async (event, connType) => {
         }, timeoutMs);
 
         port.on('data', (data) => {
-          receivedData += data.toString();
-          console.log('Received data:', data.toString());
+          receivedData = Buffer.concat([receivedData, data]); 
+          console.log('Received data:', data.toString('hex'));
 
-          if (receivedData.length >= 10) {
+          const expectedResponse = Buffer.from([0x00, 0x03, 0x45, 0x00, 0xb4, 0xc2]);
+          if (receivedData.length >= expectedResponse.length && receivedData.slice(0, expectedResponse.length).equals(expectedResponse)) {
             clearTimeout(timeout);
-            resolve(receivedData);
+            resolve(receivedData); 
           }
         });
       });
 
-      console.log('Response to message 1:', receivedData);
+      console.log('Response to message 1:', receivedData.toString('hex'));
 
       const message2 = Buffer.from([0x43, 0x4F, 0x4E, 0x46, 0x49, 0x47, 0x52, 0x45, 0x41, 0x44, 0xCF, 0xDB]);
       port.write(message2, (err) => {
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -82,52 +82,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const writeButton = document.getElementById('btn2');
-    if (writeButton) {
-        writeButton.addEventListener('click', () => {
-            const newConfig = {
-                network: {
-                    connType: parseInt(document.getElementById('connType').value),
-                    gsmMode: parseInt(document.getElementById('typeGSM').value),
-                    opMode: parseInt(document.getElementById('tcp').value),
-                    port: parseInt(document.getElementById('port').value),
-                },
-                modem: {
-                    apn: document.getElementById('apn').value,
-                    user: document.getElementById('username').value,
-                    password: document.getElementById('password').value,
-                },
-                uart1: {
-                    baudRate: parseInt(document.getElementById('baudrate1').value),
-                    dataBits: parseInt(document.getElementById('dataBits1').value),
-                    stopBits: parseInt(document.getElementById('stopBits1').value),
-                    parity: document.getElementById('parity1').value,
-                    flowControl: 'none'
-                },
-                uart2: {
-                    baudRate: parseInt(document.getElementById('baudrate2').value),
-                    dataBits: parseInt(document.getElementById('dataBits2').value),
-                    stopBits: parseInt(document.getElementById('stopBits2').value),
-                    parity: document.getElementById('parity2').value,
-                    flowControl: 'none'
-                },
-                timeout: {
-                    timeoutOpenConn: parseInt(document.getElementById('toutTCP').value),
-                    timeoutModem: parseInt(document.getElementById('toutReload').value),
-                    timeoutSim: parseInt(document.getElementById('toutSIM').value),
-                    timeoutInet: parseInt(document.getElementById('toutNet').value),
-                    timeoutTcp: parseInt(document.getElementById('toutSRV').value)
-                }
-            };
+    const readButton = document.getElementById('btn2');
+    if (readButton) {
+        readButton.addEventListener('click', () => {
+            const selectedType = document.getElementById('connType').value;
 
-            ipcRenderer.send('update-config', newConfig); // отправляем в main
-            ipcRenderer.send('write-module'); // затем отправляем команду на запись
+            const configString = collectConfigFromHtml();
+            
+            ipcRenderer.send('write-module', selectedType, configString);
+            
         });
     }
 });
 
 
+function collectConfigFromHtml() {
+    const ids = [
+        'APN', 'USER', 'PWD', 'OP_MODE', 'GSM_MODE', 
+        'PORT_SRV', 'BAUDRATE', 'DATA_SIZE', 'STOP_SIZE', 'PARITY',
+        'BAUDRATE3', 'DATA_SIZE3', 'STOP_SIZE3', 'PARITY3',
+        'TOUT_TCP', 'TOUT_RELOAD', 'TOUT_SIM', 'TOUT_NET', 'TOUT_SRV'
+    ];
 
+    let result = '';
+
+    ids.forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            result += `${id}=${element.value}\n`;
+        }
+    });
+
+    return result;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const settingsButton = document.getElementById('btn6');
